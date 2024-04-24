4dfbab58fb033b3b1ed814953496caf27da303e8
Miguel Zapata <miguel@fincrimedynamics.com>
2024-04-23 21:56:35 -0500

    create the endpoints of the service

--- src/routes.ts
+++ src/routes.ts
@@ -12,4 +12,8 @@ export const routes = (router) => {
   // TODO: Add middleware
   router.post("/api/admin/create", Create);
   router.post("/api/admin/delete", Delete);
+  router.delete("/api/user/:id", Delete);
+  router.get("/api/user/:id", GetUser);
+  router.post("/api/user", Create);
+  router.patch("/api/user/:id", Update);
 };
